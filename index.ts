1e0c243e9dbbc4d14a864f615bed96d5f4eee4bb
Pooja Kulkarni <poojakulkarni562@gmail.com>
2021-03-07 22:03:30 +0530

    Creating server for fetching users' data from their leetcode profile

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+
+import { getLeetUserProfile, getLeetQuestionsCount } from 'leet-profile-lib'
+
+const app = express()
+
+app.use(cors())
+
+app.get('/profile/:user', getLeetUserProfile)
+app.get('/questions', getLeetQuestionsCount)
+
+const port = process.env.PORT || 1100
+
+app.listen(port, () => {
+    console.log('Server listening on server https://leetcode-profile-lib.herokuapp.com/')
+})
